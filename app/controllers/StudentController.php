a1ddd6c0de7ac38c93d7e9b18c2666c26fb30e5f
yoyo930021 <yoyo930021@gmail.com>
2015-12-13 21:55:02 +0800

    修正論壇列表

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -45,7 +45,7 @@ class StudentController extends BaseController {
 	}
 
 	public function forumget($page){
-        $topics=Topic::take(10)->skip(($page-1)*10);
+        $topics=Topic::take(10)->skip(($page-1)*10)->get();
         for($i=0;$i<10;$i++){
             $writer=Student::find($topics[$i]->stu_id);
             $export[]=array(
